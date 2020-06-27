6b5dd2009c6883c2589f4ed71d5f2c6753463d70
TJ Stankus <tjstankus@gmail.com>
2020-06-27 14:30:23 -0400

    Add empty string handling
    And use a custom assertion function.

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,10 +5,22 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	expected := "Hello, TJ"
-	value := Hello("TJ")
-
-	if expected != value {
-		t.Errorf("expected %q, actual value %q", expected, value)
+	assertExpectedValue := func(t *testing.T, expected, value string) {
+		t.Helper()
+		if expected != value {
+			t.Errorf("expected %q, actual value %q", expected, value)
+		}
 	}
+
+	t.Run("say hello with name", func(t *testing.T) {
+		expected := "Hello, TJ"
+		value := Hello("TJ")
+		assertExpectedValue(t, expected, value)
+	})
+
+	t.Run("say hello with default value", func(t *testing.T) {
+		expected := "Hello, world"
+		value := Hello("")
+		assertExpectedValue(t, expected, value)
+	})
 }
