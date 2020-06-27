237a0b4bd7d591f151ce03ae86fda8dd70768677
TJ Stankus <tjstankus@gmail.com>
2020-06-27 15:00:39 -0400

    Test Spanish

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -18,9 +18,15 @@ func TestHello(t *testing.T) {
 		assertExpectedValue(t, expected, value)
 	})
 
-	t.Run("say hello with default value", func(t *testing.T) {
+	t.Run("say hello with default name value", func(t *testing.T) {
 		expected := "Hello, world"
 		value := Hello("", "English")
+		assertExpectedValue(t, expected, value)
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		expected := "Hola, TJ"
+		value := Hello("TJ", "Spanish")
 		assertExpectedValue(t, expected, value)
 	})
 }
